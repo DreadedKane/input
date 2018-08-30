57d67cdbcaa9d7582421465ff7fe968e96e67a55
Eugene Bridger <eugene.bridger@gmail.com>
2018-08-30 17:40:03 +1200

    Added README. Added project.

---
+++ Assets/HomewreckersStudio/Input/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * Copyright (c) Eugene Bridger. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
+using UnityEngine;
+
+namespace HomewreckersStudio
+{
+    public sealed partial class InputManager : MonoBehaviour
+    {
+        partial void Internal();
+
+        /**
+         * Defers to installed InputManager.
+         */
+        private void Awake()
+        {
+            Internal();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Input
+
+Unity package for input management.
+
+## Requirements
+
+### Rewired
+* [Rewired](https://assetstore.unity.com/packages/tools/utilities/rewired-21676)
+
+## License
+[MIT](https://choosealicense.com/licenses/mit)
